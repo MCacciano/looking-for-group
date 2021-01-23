0c6e4ea19f88d224966f14836c4f19d3edf67a2d
Michael <michael.cacciano@gmail.com>
2021-01-22 23:11:47 -0500

    fetching villagers successfully from ACNH API, currently just displaying them in main App component sorted alphabetically

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
+import acnhApi from './axios/acnhApi';
+
 const App = () => {
+  const [villagers, setVillagers] = useState([]);
+
+  useEffect(() => {
+    const getVillagers = async () => {
+      try {
+        const { data } = await acnhApi.get('/villagers');
+        console.log('data :>> ', data);
+        const sorted = data.sort((a, b) =>
+          a.name['name-USen'].localeCompare(b.name['name-USen'], 'en')
+        );
+        setVillagers(sorted);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getVillagers();
+
+    return null;
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl text-blue-600">App</h1>
+    <div className='m-4'>
+      <div className='grid grid-cols-4 gap-6'>
+        {villagers.map(({ name, icon_uri, image_uri }) => (
+          <div className='flex flex-col items-center border border-gray-500 rounded shadow p-2 bg-gray-100'>
+            <div>
+              <img src={icon_uri} />
+            </div>
+            <h2>{name['name-USen']}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
