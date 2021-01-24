92c47eb4d907fa98e69de12134cd7f9dddf96047
Michael <michael.cacciano@gmail.com>
2021-01-23 20:22:59 -0500

    added firebase config and google sign in function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import { useEffect, useState } from 'react';
 import acnhApi from './axios/acnhApi';
+import { auth, signInWithGoogle } from './firebase';
 
 const App = () => {
+  const [user, setUser] = useState(null);
   const [villagers, setVillagers] = useState([]);
 
   useEffect(() => {
-    const getVillagers = async () => {
-      try {
-        const { data } = await acnhApi.get('/villagers');
-        console.log('data :>> ', data);
-        data.sort((a, b) => a.name['name-USen'].localeCompare(b.name['name-USen'], 'en'));
-        setVillagers(data);
-      } catch (err) {
-        console.error(err);
+    const unsub = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log('authUser :>> ', authUser);
+      } else {
+        setUser(null);
       }
-    };
+    });
 
-    getVillagers();
-
-    return null;
+    return unsub;
   }, []);
+
+  // useEffect(() => {
+  //   const getVillagers = async () => {
+  //     try {
+  //       const { data } = await acnhApi.get('/villagers');
+  //       console.log('data :>> ', data);
+  //       data.sort((a, b) => a.name['name-USen'].localeCompare(b.name['name-USen'], 'en'));
+  //       setVillagers(data);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getVillagers();
+
+  //   return null;
+  // }, []);
 
   return (
     <div className='m-4'>
-      <div className='grid grid-cols-4 gap-6'>
+      <button
+        type='button'
+        onClick={signInWithGoogle}
+        className='text-white border border-black bg-blue-600 cursor-pointer shadow p-2 mr-6 rounded'
+      >
+        Sign In With Google
+      </button>
+      <button
+        type='button'
+        onClick={() => auth.signOut()}
+        className='text-white border border-black bg-red-600 cursor-pointer shadow p-2 rounded'
+      >
+        Logout
+      </button>
+      <ul className='grid grid-cols-4 gap-6 list-none'>
         {villagers.map(({ name, icon_uri, image_uri }) => (
-          <div className='flex flex-col items-center border border-gray-500 rounded shadow p-2 bg-gray-100'>
+          <li className='flex flex-col items-center border border-gray-500 rounded shadow p-2 bg-gray-100'>
             <div>
-              <img src={image_uri} />
               <img src={icon_uri} />
             </div>
             <h2>{name['name-USen']}</h2>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from './firebase';
 
 import './tailwind.css';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (firebase.apps.length) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
